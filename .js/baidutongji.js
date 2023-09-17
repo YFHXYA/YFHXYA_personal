7f7aee59d8947dd7b225c612bacc39691736d096
菰城鸥鹭 <qq3370291812@outlook.com>
2023-09-17 05:01:28 +0000

    add .js/baidutongji.js.
    Signed-off-by: 菰城鸥鹭 <qq3370291812@outlook.com>

---
+++ .js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?15b7fcb3362d68978dcb0b9e1efcb065";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
